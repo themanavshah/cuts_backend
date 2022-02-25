35f0200fbecb0c42b641ee7afcf069ed88bbda7d
Manav Shah <76565191+themanavshah@users.noreply.github.com>
2022-02-25 22:50:07 +0530

    Update payment.js

--- stripe_payments/payment.js
+++ stripe_payments/payment.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')("sk_test_51JFzWkSJFrMMx7cfaC2Bkxewi2cPMupCoTTmPioilIDvcSni3ZmWkpPyzJhsPk9K6WdNBFBDJWSKL7HI2sIma397000UKMtO2d")
+const stripe = require('stripe')(stipeSKTestKey)
 
 exports.stripe_payment = async (res, req, next) => {
     const stripeVendorAccount = 'acct_1JFzWkSJFrMMx7cf';
